7d7c3577985b94d3910825a67540ab5728bc2b3a
klaus <kodemode@wp.pl>
2020-11-30 13:33:52 +0100

    tablice lekcja 2

---
+++ tablice2/zadanie 31.ts
@@ -0,0 +1,22 @@
+// const imie = ['Anna', 'Adam', 'Piotr', 'Ewa'];
+// console.log(imie.reverse().join('\n'));
+
+// const liczby = [100, 20, 10, 1000, 200, 2];
+// liczby.sort((a, b) => a - b);
+// console.table(liczby);
+
+// const objec = {
+//   id: 1243543253453,
+//   userName: "anonim",
+//   email: "anka@manka.interia.pl"
+// };
+// console.table(objec);
+
+// ? 3.2
+
+// const tabliczka = [2, 'b', 'g', 'a', 1];
+// const str = tabliczka.splice(1, 3).sort();
+// const newTabliczka: (string | number)[] = [...str, ...tabliczka.sort((a: number, b: number) => a - b)];
+
+// console.log(newTabliczka);
+
