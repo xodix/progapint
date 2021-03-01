578fb9e68e813f5425e8a5b5fee76fd7ef5c4a9b
xodix <kodemode@wp.pl>
2021-03-01 10:36:15 +0100

    eslint

---
+++ obiekty dom/1.js
@@ -0,0 +1,11 @@
+"use strict";
+// !obsługa zdarzeń w JS
+// ? Czym jest zdarzenie
+// najważniejsze zdarzenia w modelu dom
+// sposoby rejestracji funkcji obsługujących zdarzenia
+//   inline
+//   funkcja
+//   funkcja anonimowa
+//   nasłuchwanie zdarzeń addEventListener(event, function)
+console.log("hello world");
+console.log("hello world");
