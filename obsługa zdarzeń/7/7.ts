36dca7ed596717125d4ee7bfee87ab09ba5c5bd2
xodix <kodemode@wp.pl>
2021-03-15 11:34:38 +0100

    7

---
+++ obsługa zdarzeń/7/7.ts
@@ -0,0 +1,16 @@
+const l = document.getElementsByName("lc")[0] as HTMLInputElement;
+const p = document.getElementsByName("pc")[0] as HTMLInputElement;
+document.getElementsByTagName("form")[0].addEventListener("submit", (e) => {
+  e.preventDefault();
+  const koszt = document.getElementById("koszt");
+  const val = +l.value;
+  const isPremium = p.checked;
+  const cost = isPremium ? val * 2.6 : val * 2;
+  const weight = isPremium ? val * 1.75 : val * 1.5;
+  const ik = +(e.target as HTMLFormElement).ik.value;
+  koszt!.textContent = `Zakupiona ilość cegieł: ${val}, ${
+    isPremium ? "cegła premium" : "cegła standardowa"
+  }, koszt zakupu cegieł: ${cost}, waga cegieł: ${weight}, koszt treansportu: ${
+    ik * Math.floor(weight / 10) * 0.5
+  }`;
+});
